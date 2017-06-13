acb34cf3fa9dda2383819bea070ef022e1902852
Ryuhei-Takeda <ryutake@shirai.la>
2017-06-14 02:34:21 +0900

    UnitySoundProject001
    Lecture.5

---
+++ Assets/BoardMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardMove : MonoBehaviour {
+
+    //時間管理//
+    public float time;
+
+
+	// Use this for initialization
+	void Start () {
+        
+        //timeの初期化//
+        time = 0;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //時間の更新//
+        time += Time.deltaTime;
+
+        transform.localPosition = new Vector3(0,  - time * 100, 0);
+
+	}
+}
